0f6ffd3a0852a40a8db501813ea69d6b72615004
yukio <143847357+yukio-kayaba@users.noreply.github.com>
2024-11-13 12:50:52 -0500

    subiendo detallles
    dada

--- hotel_proyecto_sky/index.php
+++ hotel_proyecto_sky/index.php
@@ -2,8 +2,11 @@
 	require_once("config.php");
 	require_once("controller/controlador.php");
 	if(isset($_GET['vista'])){
-		if(method_exists("ModeloControlador",$_GET['vista'])){
-			ModeloControlador::{$_GET["vista"]}();
+		$url1 = explode("/",$_GET['vista']);
+		if(method_exists("ModeloControlador",$url1[0])){
+			// ModeloControlador::{$url[0]}();
+			// print("variable "+$url[0]);
+			// ModeloControlador::{$url1[0]}();
 		} else {
 			ModeloControlador::error_pagina();
 		}
--- hotel_proyecto_sky/view/layouts/titulo_principal.php
+++ hotel_proyecto_sky/view/layouts/titulo_principal.php
@@ -1,18 +1,17 @@
+
 <section class="section parallax bg-image-1">
     <div class="titulo_principal">
-        <img style="--i:0" src="<?php echo url;?>/view/images/hotel_loe.gif" alt="">
+        <img  src="<?php echo url;?>/view/images/hotel_loe.gif" alt="">
+        <p> Bienvenido a  </p>
         <h2>
-            <span style="--i:1">H</span>
-            <span style="--i:2">O</span>
-            <span style="--i:3">T</span>
-            <span style="--i:4">E</span>
-            <span style="--i:5">L</span>
-            <span style="--i:6"> </span>
-            <span style="--i:7">V</span>
-            <span style="--i:8">I</span>
-            <span style="--i:9">L</span>
-            <span style="--i:10">L</span>
-            <span style="--i:11">A</span>
+            <?php 
+                $texto = "HOTEL VILLA"; 
+
+                for ($i=0; $i < strlen($texto); $i++) { 
+            ?>
+                <span style="--i:<?php echo ($i + 2);?>"><?php echo $texto[$i];?></span>
+            <?php }?>
         </h2>
+        <p>Su Comodidad al maximo</p>
     </div>
 </section>
