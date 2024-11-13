464497a4e67c1f1f11af8ad06c8f10a0ac9340a5
yukio <zviamontevilca@gmail.com>
2024-11-13 17:56:07 -0500

    actualizando
    datos_elementos

--- hotel_proyecto_sky/view/layouts/titulo_principal.php
+++ hotel_proyecto_sky/view/layouts/titulo_principal.php
@@ -14,4 +14,19 @@
         </h2>
         <p>Su Comodidad al maximo</p>
     </div>
+    <div class="elemento_separado">
+        <div>
+            <p>Iniciar Secion</p>
+        </div>
+        <div>
+            <p>Reservar Fecha</p>
+        </div>
+        <div>
+            <p>Hubicacion</p>
+        </div>
+
+    </div>
+</section>
+<section class="section parallax bg-image-1">
+
 </section>
