0fc1ed05cb5452cbcd614f61483030990079eaec
yukio <zviamontevilca@gmail.com>
2024-11-14 15:49:18 -0500

    subiendo actualizaciones de gps
    se esta subiendo cambios como integracion del gps

--- hotel_proyecto_sky/view/layouts/titulo_principal.php
+++ hotel_proyecto_sky/view/layouts/titulo_principal.php
@@ -24,9 +24,9 @@
         <div>
             <p>Hubicacion</p>
         </div>
-
     </div>
 </section>
 <section class="section parallax bg-image-1">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15575.352729069447!2d-69.20430054999999!3d-12.59291425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x917b4eadc297b061%3A0xab262fb284ada8fd!2sPlaza%20Bolognesi!5e0!3m2!1ses-419!2spe!4v1731617117242!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 </section>
