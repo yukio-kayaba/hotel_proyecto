b6eeb28d6a308965c4cbf23d67ae14c4eec48d22
YOR-ROY <137833595+Schrodinger314@users.noreply.github.com>
2024-11-16 13:18:25 -0500

    haciendo servicios
    solo servicios

--- hotel_proyecto_sky/view/inicio.php
+++ hotel_proyecto_sky/view/inicio.php
@@ -5,6 +5,7 @@
 <?php include("layouts/head.php"); ?>
     <link rel="stylesheet" href="<?php echo url;?>/view/css/navbar_estyle.css">
     <link rel="stylesheet" href="<?php echo url;?>/view/css/titulo_principal.css">
+    <link rel="stylesheet" href="<?php echo url;?>/view/css/servcios.css">
 </head>
 <body>
     <?php include("layouts/navbar.php"); ?>
--- hotel_proyecto_sky/view/layouts/page_date.php
+++ hotel_proyecto_sky/view/layouts/page_date.php
@@ -1,1 +1,115 @@
-<p>hola mundo</p>
+<section class="servicios">
+    <div class="container">
+        <div class="titulo-servicios">
+            <h2>Nuestros servicios</h2>
+        </div>
+        <div class="row">
+            <!-- Servicio 1 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=OdeDzIaTFmYC&format=png&color=000000" alt="Excelente ubicación">
+                    <h5>Excelente ubicación</h5>
+                </div>
+            </div>
+            <!-- Servicio 2 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=3egeYFlD0GQb&format=png&color=000000" alt="Room Service">
+                    <h5>Room Service</h5>
+                </div>
+            </div>
+            <!-- Servicio 3 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=jdfSzki71PSi&format=png&color=000000" alt="Recepción 24h">
+                    <h5>Recepción 24h</h5>
+                </div>
+            </div>
+            <!-- Servicio 4 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=wKsS7axWwAgz&format=png&color=000000" alt="Pagos digitales">
+                    <h5>Pagos digitales</h5>
+                </div>
+            </div>
+            <!-- Servicio 5 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=lsb2u6phvREO&format=png&color=000000" alt="Guarda equipaje">
+                    <h5>Guarda equipaje</h5>
+                </div>
+            </div>
+            <!-- Servicio 6 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=emQp0VZNpjef&format=png&color=000000" alt="Servicio de despertador">
+                    <h5>Servicio de despertador</h5>
+                </div>
+            </div>
+            <!-- Servicio 7 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=M4W4VwEUUii2&format=png&color=000000" alt="Wifi">
+                    <h5>Wifi</h5>
+                </div>
+            </div>
+            <!-- Servicio 8 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=QKmVEj7QaMCS&format=png&color=000000" alt="Desayuno">
+                    <h5>Desayuno</h5>
+                </div>
+            </div>
+            <!-- Servicio 9 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=eGPwwh8A1vK9&format=png&color=000000" alt="Restaurante">
+                    <h5>Restaurante</h5>
+                </div>
+            </div>
+            <!-- Servicio 10 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=oLeZ2JpuP12B&format=png&color=000000" alt="Bar">
+                    <h5>Bar</h5>
+                </div>
+            </div>
+            <!-- Servicio 11 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=yBd3Dr4lwlqo&format=png&color=000000" alt="Piscina">
+                    <h5>Piscina</h5>
+                </div>
+            </div>
+            <!-- Servicio 12 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=y1mUejqAHJKv&format=png&color=000000" alt="Estacionamiento Exterior">
+                    <h5>Estacionamiento</h5>
+                </div>
+            </div>
+            <!-- Servicio 13 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=MGpnKPJ3YdeF&format=png&color=000000" alt="Traslados">
+                    <h5>Traslados</h5>
+                </div>
+            </div>
+            <!-- Servicio 14 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=hs1vV9dKOHH6&format=png&color=000000" alt="Seguridad">
+                    <h5>Seguridad</h5>
+                </div>
+            </div>
+            <!-- Servicio 15 -->
+            <div class="col">
+                <div class="icon-box">
+                    <img src="https://img.icons8.com/?size=100&id=vyQ7GOrIhONy&format=png&color=000000" alt="Bicicletas">
+                    <h5>Bicicletas</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
