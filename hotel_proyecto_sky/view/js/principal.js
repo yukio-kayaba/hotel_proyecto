bbc6d777bde762ebe754ba09ef7924fee6f9ed19
yukio <zviamontevilca@gmail.com>
2024-11-20 10:34:19 -0500

    navbar desplegable
    el navbar ahora se desplaza

---
+++ hotel_proyecto_sky/view/js/principal.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll",()=>{
+    let nav_bar = document.getElementById("nav-bar");
+    // if(window.scrollY > 50)){
+    //     nav_bar.classList.toggle("nav-colores");
+    // }
+
+    nav_bar.classList.toggle("nav-colores",window.scrollY > 50);
+    // if(window.scrollY < 150){
+    // }else{
+    // }
+
+});
